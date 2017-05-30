a1536b473d6e1af9bd72f92d61be83e4201b8d23
lianshuideyu <834893479@qq.com>
2017-05-30 20:56:46 +0800

    RecyclerVie_MobilePlay

---
+++ app/src/main/java/com/atguigu/recyclerview_mobileplay/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package com.atguigu.recyclerview_mobileplay;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+
+
+}
